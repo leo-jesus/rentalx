3105b77a5f6ec1e29b1a17068a0e2e38a1154088
leo-jesus <llxdgb1@gmail.com>
2021-04-06 00:00:23 -0300

    creating users seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,9 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../typeorm";
-import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+import "@shared/container";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
