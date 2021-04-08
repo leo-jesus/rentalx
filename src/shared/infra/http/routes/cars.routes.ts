5f60ccb7354689d417e1ef733ae08da093da6b4d
leo-jesus <llxdgb1@gmail.com>
2021-04-07 22:47:48 -0300

    listing available cars by name, brand or category.

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCars/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAutheticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
+
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
     "/",
@@ -14,5 +17,7 @@ carsRoutes.post(
     ensureAdmin,
     createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
