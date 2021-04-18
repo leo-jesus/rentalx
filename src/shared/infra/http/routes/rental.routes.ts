3ccc79e7851365ae58ecbcb6c1c2a8172dd302f6
leo-jesus <llxdgb1@gmail.com>
2021-04-17 21:31:14 -0300

    Created devolution rental module

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAutheticated } from "../middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.post("/", ensureAutheticated, createRentalController.handle);
+rentalsRoutes.post(
+    "/devolution/:id",
+    ensureAutheticated,
+    devolutionRentalController.handle
+);
 
 export { rentalsRoutes };
